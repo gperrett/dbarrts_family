49b6f9c84ab5e87c829a1a08ff1147917198e0fb
George Perrett <gjperrett94@gmail.com>
2023-05-08 08:14:17 -0400

    updated for cluster

--- stan4bart/fit.R
+++ stan4bart/fit.R
@@ -6,175 +6,178 @@ source('draw.R')
 source('pull_estimates.R')
 source('pull_convergence .R')
 
+
+iteration <- Sys.getenv('SLURM_ARRAY_TASK_ID')
+
+set.seed(iteration)
+
 sim <- draw_dat()
 
+worlds <- c('worldA', 'worldB', 'worldC')
 
-m1 <- stan4bart::stan4bart(y ~ bart(. -schoolid -ID) + (1|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
+results <- lapply(worlds, function(world){
 
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m1)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m1)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m1))
-
-
-m2 <- stan4bart::stan4bart(y ~ . -schoolid -ID + bart(. -schoolid -ID) + (1|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m2)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m2)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m2)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m2))
-
-
-
-m3 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID) + (1|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m3)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m3)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m3)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m3))
-
-
-
-m4 <- stan4bart::stan4bart(y ~ bart(. -schoolid -ID) + (z|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m4)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m4)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m4)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m4))
-
-
-m5 <- stan4bart::stan4bart(y ~ . -schoolid -ID + bart(. -schoolid -ID) + (z|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m5)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m5)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m5)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m5))
-
-
-
-m6 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID) + (z|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m6)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m6)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m6)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m6))
-
-
-m7 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID -z) + (z|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m7)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m7)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m7)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m7))
-
-m8 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID -z) + (1|schoolid),
-                           data = sim[['worldA']][["data"]],
-                           treatment = z,
-                           cores = 1,
-                           chains = 10,
-                           iter = 4000
-)
-
-sim[['worldA']][["satt"]] <- cbind(sim[['worldA']][["satt"]], pull_estimates(fit = m8)[['satt_est']])
-
-sim[["worldA"]][["gsatt"]] <- cbind(sim[["worldA"]][["gsatt"]],
-                                    merge(sim[['worldA']][["gsatt"]], pull_estimates(fit = m8)[['gsatt_est']]))
-
-sim[["worldA"]][["icatt"]] <- cbind(sim[["worldA"]][["icatt"]], 
-                                    merge(sim[['worldA']][["icatt"]], pull_estimates(fit = m8)[['icatt_est']]))
-
-sim[["worldA"]][['rhat']] <- rbind(sim[["worldA"]][['rhat']], 
-                                   pull_convergence(fit = m8))
+    m1 <- stan4bart::stan4bart(y ~ bart(. -schoolid -ID) + (1|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m1)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m1))
+  
+  rm(m2)
+  gc()
+  m2 <- stan4bart::stan4bart(y ~ . -schoolid -ID + bart(. -schoolid -ID) + (1|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m2)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m2))
+  
+  
+  rm(m2)
+  gc()
+  m3 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID) + (1|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m3)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m3))
+  
+  
+  rm(m3)
+  gc()
+  m4 <- stan4bart::stan4bart(y ~ bart(. -schoolid -ID) + (z|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m4)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m4))
+  
+  rm(m4)
+  gc()
+  m5 <- stan4bart::stan4bart(y ~ . -schoolid -ID + bart(. -schoolid -ID) + (z|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m5)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m5))
+  
+  
+  rm(m5)
+  gc()
+  m6 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID) + (z|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m6)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m6))
+  
+  rm(m6)
+  gc()
+  m7 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID -z) + (z|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m7)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m7))
+  
+  rm(m7)
+  gc()
+  
+  m8 <- stan4bart::stan4bart(y ~ z + bart(. -schoolid -ID -z) + (1|schoolid),
+                             data = sim[[world]][["data"]],
+                             treatment = z,
+                             cores = 1,
+                             chains = 10,
+                             iter = 4000
+  )
+  
+  sim[[world]][["satt"]] <<- cbind(sim[[world]][["satt"]], pull_estimates(fit = m8)[['satt_est']])
+  
+  sim[[world]][["gsatt"]] <<- merge(sim[[world]][["gsatt"]], pull_estimates(fit = m1)[['gsatt_est']])
+  
+  sim[[world]][["icatt"]] <<- merge(sim[[world]][["icatt"]], pull_estimates(fit = m1)[['icatt_est']])
+  
+  sim[[world]][['rhat']] <<- rbind(sim[[world]][['rhat']], 
+                                     pull_convergence(fit = m8))
 
 
+})
 
 
-write_rds(dat, glue::glue('results/results_iter{iter}.rds'), compress = 'gz')
+write_rds(results, glue::glue('results/results_iter{iteration}.rds'), compress = 'gz')
 
 
 
